0fc01e134339e978d002b39a49ba232b88cab748
Teniola876 <awodireteniola4@gmail.com>
2025-09-09 23:53:31 +0100

    first commit

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,186 @@
+import React, { useRef, useState, useEffect } from "react";
+import "./Quiz.css";
+import quizData from "../../assets/data";
+
+const Quiz = () => {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(quizData[index]);
+  let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0);
+  let [count, setCount] = useState(60);
+  let [result, setResult] = useState(false);
+  let [fullName, setFullname] = useState("");
+  const user = localStorage.getItem("user");
+
+  useEffect(() => {
+
+    if (count > 0 && user ) {
+      setTimeout(() => {
+        setCount(count - 1);
+      }, 500);
+    } else {
+      clearInterval(count);
+    }
+  }, [count]);
+
+  // let [user,store] = useState
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1, Option2, Option3, Option4];
+
+  const checkAns = (e, ans) => {
+    if (lock === false) {
+      if (question.ans === ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore((prev) => prev + 1);
+      } else {
+        e.target.classList.add("incorrect");
+        setLock(true);
+        option_array[question.ans - 1].current.classList.add("correct");
+      }
+    }
+  };
+
+  const reset = () => {
+    setIndex(0);
+    setQuestion(quizData[0]);
+    setScore(0);
+    setLock(false);
+    setResult(false);
+    setCount(60);
+    localStorage.removeItem("user");
+  };
+
+  const next = () => {
+    if (lock === true) {
+      if (index === quizData.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(quizData[index]);
+      setLock(false);
+      option_array.map((option) => {
+        option.current.classList.remove("incorrect");
+        option.current.classList.remove("correct");
+        return null;
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("user", fullName);
+    window.location.reload()
+  };
+
+
+  return (
+    <div className="container">
+      {user && <h1> {count}</h1>}
+      
+      <h1>Quiz App</h1>
+
+      {!user ? (
+        
+        <form onSubmit={handleSubmit}>
+             <div className="enter">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="fullName"
+            value={fullName}
+            onChange={(e) => setFullname(e.target.value)}
+          />
+       
+          <button>Enter</button>
+        </div>
+        </form>
+      ) : (
+        <></>
+      )}
+
+     
+
+      {user && (
+        <>
+          <hr />
+          {!count ? (
+            <></>
+          ) : (
+            <>
+              <h2>
+                {index + 1}. {question.question}
+              </h2>
+
+              <>
+                <ul>
+                  <li
+                    ref={Option1}
+                    onClick={(e) => {
+                      checkAns(e, 1);
+                    }}
+                  >
+                    {question.option1}
+                  </li>
+                  <li
+                    ref={Option2}
+                    onClick={(e) => {
+                      checkAns(e, 2);
+                    }}
+                  >
+                    {question.option2}
+                  </li>
+                  <li
+                    ref={Option3}
+                    onClick={(e) => {
+                      checkAns(e, 3);
+                    }}
+                  >
+                    {question.option3}
+                  </li>
+                  <li
+                    ref={Option4}
+                    onClick={(e) => {
+                      checkAns(e, 4);
+                    }}
+                  >
+                    {question.option4}
+                  </li>
+                </ul>
+                {/* {count > 0 ? ( <button onClick={next}>Next</button>): (<h2>Time up</h2>)} */}
+                <button onClick={next}>Next</button>
+              </>
+
+              <div className="index">
+                {index + 1} of {quizData.length} questions
+              </div>
+            </>
+          )}
+
+          {!count ? (
+            <>
+              <h2>
+              {user}  You Scored {score} out of {quizData.length}
+              </h2>
+              <button onClick={reset}> Reset </button>
+            </>
+          ) : (
+            <></>
+          )}
+          {/* <h2>
+            You Scored {score} out of {quizData.length}
+          </h2>
+          <button onClick={reset}> Reset </button> */}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
---
+++ src/assets/data.js
@@ -0,0 +1,87 @@
+// data.js
+
+ export const quizData = [
+   {
+      question: "Which planet is known as the Red Planet?",
+      option1: "Venus",
+      option2: "Mars",
+      option3: "Jupiter",
+      option4: "Saturn",
+      ans: 2,
+    },
+    {
+      question: "Who is the current President of Nigeria (2025)?",
+      option1: "Muhammadu Buhari",
+      option2: "Bola Ahmed Tinubu",
+      option3: "Goodluck Jonathan",
+      option4: "Atiku Abubakar",
+      ans: 2,
+    },
+    {
+      question: "Which programming language is known as the backbone of the web?",
+      option1: "Python",
+      option2: "Java",
+      option3: "JavaScript",
+      option4: "C++",
+      ans: 3,
+    },
+    {
+      question: "What is the capital of Lagos State?",
+      option1: "Lekki",
+      option2: "Ikeja",
+      option3: "Ikorodu",
+      option4: "Epe",
+      ans: 2,
+    },
+    {
+      question: "Which social media app is owned by Elon Musk?",
+      option1: "Instagram",
+      option2: "X (formerly Twitter)",
+      option3: "Snapchat",
+      option4: "TikTok",
+      ans: 2,
+    },
+    {
+      question: "What does CSS stand for?",
+      option1: "Computer Style Sheets",
+      option2: "Creative Style Sheets",
+      option3: "Cascading Style Sheets",
+      option4: "Colorful Style Sheets",
+      ans: 3,
+    },
+    {
+      question: "Which company owns the Android operating system?",
+      option1: "Apple",
+      option2: "Google",
+      option3: "Microsoft",
+      option4: "Samsung",
+      ans: 2,
+    },
+    {
+      question: "Which company created the iPhone?",
+      option1: "Samsung",
+      option2: "Google",
+      option3: "Apple",
+      option4: "Microsoft",
+      ans: 3,
+    },
+    {
+      question: "What does HTML stand for?",
+      option1: "Hyper Text Markup Language",
+      option2: "High Text Machine Language",
+      option3: "Hyperlinks and Text Making Language",
+      option4: "Home Tool Markup Language",
+      ans: 1,
+    },
+    {
+      question: "What year did Nigeria gain independence?",
+      option1: "1960",
+      option2: "1970",
+      option3: "1950",
+      option4: "1963",
+      ans: 1
+    }
+  ];
+  
+  export default quizData;
+  
